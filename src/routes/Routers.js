b5b0e1aee8d2f7e8463bddca8543866a3246551b
Marcelo Ferreira <marcelohpf@gmail.com>
2017-08-26 14:23:35 -0300

    Create data table for client

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -6,13 +6,14 @@ import LogoutButton from '../auth/LogoutButton';
 import PasswordForm from '../auth/form/PasswordForm';
 import ResetForm from '../auth/ResetForm';
 import {AuthorizedRoute, NotFoundRoute} from './Router';
+import ClientTable from '../client/ClientTable';
 
 export default class Routers extends Component{
 
   render(){
     return (
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/" render={() => <ClientTable />} />
         <Route exact path="/login" component={ LoginPage } />
         <AuthorizedRoute path="/logout" component={ LogoutButton } />
         <AuthorizedRoute path="/login/changepassword" render={ () => <PasswordForm userid={3} username="Marcelo" /> } />
