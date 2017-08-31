eb534fceeb271f8ac97996a6de09e99938fe570e
Marcelo Ferreira <marcelohpf@gmail.com>
2017-08-30 23:14:26 -0300

    Add table for client with sort and filter

--- src/resources/getData.js
+++ src/resources/getData.js
@@ -1,13 +1,16 @@
 import {Auth} from '../auth/Auth';
 
-const getData = (url, component, field) => {
+const getData = (url, component, field, fieldB) => {
   console.log(field);
   fetch(url, {
     method: 'get',
     headers: Auth.getHeader(),
   })
   .then((response) => response.json() )
-  .then((data) => {component.setState({[field]: data});})
+  .then((data) => {
+    component.setState({[field]: data});
+    if (fieldB !== undefined) component.setState({[fieldB]: data});
+  })
   .catch((error) => {console.log(error);});
 };
 
