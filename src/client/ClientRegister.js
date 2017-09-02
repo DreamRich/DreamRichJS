79c1e9b68ad599264d6c4ea0885d3c57813c8b23
Icaro Pires <icaropiresdf@gmail.com>
2017-09-01 23:42:53 -0300

    Add all fields to ClientRegister form
    Signed-off-by: Icaro Pires <icaropiresdf@gmail.com>

--- src/client/ClientRegister.js
+++ src/client/ClientRegister.js
@@ -4,18 +4,19 @@ import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import {FormsyText, FormsyDate} from 'formsy-material-ui/lib';
 import errorMessages from '../utils/FormsErrorMessages';
+import IconButton from 'material-ui/IconButton';
+import FileFileUpload from 'material-ui/svg-icons/file/file-upload';
 import '../stylesheet/RegisterForms.sass';
 
-class ClientRegister extends Component{
+class ClientRegister extends Component {
 
   constructor(props){
-
     super(props);
     this.state = {
       canSubmit: false,
     };
   }
- 
+
   enableButton() {
     this.setState({
       canSubmit: true,
@@ -48,7 +49,7 @@ class ClientRegister extends Component{
           }
           else if(i == buttons.lenght - 1) {
             console.error(form_name  +
-                          ' doesn\'t have any button for partial submit');
+              ' doesn\'t have any button for partial submit');
           }
         }
       }
@@ -62,8 +63,8 @@ class ClientRegister extends Component{
         method: 'post',
         body: data
       })
-      .then(() => {console.log(form_name + ' was submitted');})
-      .catch(() => {console.error('An error occurred when submitting ' + form_name);});
+        .then(() => {console.log(form_name + ' was submitted');})
+        .catch(() => {console.error('An error occurred when submitting ' + form_name);});
 
       count++;
     }
@@ -88,117 +89,318 @@ class ClientRegister extends Component{
       <div>
         <h1> Cadastro de Cliente </h1>
 
-          <Paper className="Paper">
-            <Formsy.Form
-              name="active_client_form"
-              onValid={this.enableButton.bind(this)}
-              onInvalidSubmit={this.notifyFormError}
-              onValidSubmit={submitForm.bind(this)}>
+        <Paper className="Paper">
+          <Formsy.Form
+            name="client"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
 
-              <input
-                hidden
-                readOnly
-                name="route"
-                type="text"
-                value="client/active/"
-              / >
-              <FormsyText
-                name="name"
-                validations="isWords"
-                validationError={wordsError}
-                hintText="Qual é o seu nome?"
-                floatingLabelText="Nome"
-                required
-              />
-              <FormsyText
-                name="surname"
-                validations="isWords"
-                validationError={wordsError}
-                hintText="Qual é o seu sobrenome?"
-                floatingLabelText="Sobrenome"
-                required
-              />
-              <FormsyDate
-                name="birthday"
-                hintText="Data de nascimento"
-                floatingLabelText="Clique para selecionar"
-                required
-              />
-              <FormsyText
-                name="profession"
-                validations="isWords"
-                validationError={wordsError}
-                hintText="Com o que você trabalha?"
-                floatingLabelText="Profissão"
-                required
-              />
-              <FormsyText
-                name="cpf"
-                validations="isNumeric"
-                validationError={numericError}
-                hintText="Apenas números"
-                floatingLabelText="CPF"
-                updateImmediately
-                required
-              />
-              <FormsyText
-                name="telephone"
-                validations="isNumeric"
-                validationError={numericError}
-                hintText="Insira seu telefone"
-                floatingLabelText="Telefone"
-                updateImmediately
-                required
-              />
-              <FormsyText
-                name="email"
-                validations="isEmail"
-                validationError={emailError}
-                hintText="Insira seu e-mail"
-                floatingLabelText="E-mail"
-                required
-              />
-              <FormsyText
-                name="hometown"
-                validations="isWords"
-                validationError={wordsError}
-                hintText="Onde você nasceu?"
-                floatingLabelText="Cidade natal"
-                required
-              />
-              <button hidden name="partial_submit" type="submit" />
-            </Formsy.Form>
+            <input
+              name="route"
+              type="text"
+              value="client/"
+              hidden
+              readOnly
+            / >
+            <FormsyText
+              name="name"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Nome do cliente"
+              floatingLabelText="Nome"
+              required
+            />
+            <FormsyText
+              name="surname"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Sobrenome do cliente"
+              floatingLabelText="Sobrenome"
+              required
+            />
+            <FormsyDate
+              name="birthday"
+              hintText="Data de nasc. do cliente"
+              floatingLabelText="Data de Nascimento"
+              required
+            />
+            <FormsyText
+              name="profession"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Profissão do cliente"
+              floatingLabelText="Profissão"
+              required
+            />
+            <FormsyText
+              name="cpf"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Apenas números"
+              floatingLabelText="CPF"
+              updateImmediately
+              required
+            />
+            <FormsyText
+              name="telephone"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Telefone do cliente"
+              floatingLabelText="Telefone"
+              updateImmediately
+              required
+            />
+            <FormsyText
+              name="email"
+              validations="isEmail"
+              validationError={emailError}
+              hintText="E-mail do cliente"
+              floatingLabelText="E-mail"
+              required
+            />
+            <FormsyText
+              name="hometown"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Onde o cliente nasceu?"
+              floatingLabelText="Cidade natal"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
 
-            <Formsy.Form
-              name="client_form"
-              onValid={this.enableButton.bind(this)}
-              onInvalidSubmit={this.notifyFormError}
-              onValidSubmit={submitForm.bind(this)}>
-              <input
-                hidden
-                readOnly
-                name="route"
-                type="text"
-                value="client/"
-              / >
-              <FormsyText
-                name="name"
-                validations="isWords"
-                validationError={wordsError}
-                hintText="Qual é o seu nome?"
-                floatingLabelText="Nome"
-                required
-              />
-              <button hidden name="partial_submit" type="submit" />
-            </Formsy.Form>
-              <RaisedButton
-                primary
-                type="submit"
-                label="Enviar"
-                onClick={partialSubmitForms.bind(this)}
-                disabled={!this.state.canSubmit}
-              />
-          </Paper>
+          <Formsy.Form
+            name="address"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              name="route"
+              type="text"
+              value="client/address/"
+              hidden
+              readOnly
+            / >
+            <FormsyText
+              name="cep"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Apenas números"
+              floatingLabelText="CEP"
+              updateImmediately
+              required
+            />
+            <FormsyText
+              name="details"
+              validations="isWords" 
+              validationError={wordsError}
+              hintText="Detalhes do endereço"
+              floatingLabelText="Detalhes"
+            />
+            <FormsyText
+              name="number"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Número do lote"
+              floatingLabelText="Número"
+              updateImmediately
+              required
+            />
+            <FormsyText
+              name="complement"
+              validations="isWords" 
+              validationError={wordsError}
+              hintText="Complemento do endereço"
+              floatingLabelText="Complemento"
+              required
+            />
+            <FormsyText
+              name="neighborhood"
+              validations="isWords" 
+              validationError={wordsError}
+              hintText="Bairro do endereço"
+              floatingLabelText="Bairro"
+              required
+            />
+            <FormsyText
+              name="type_of_address"
+              validations="isWords" 
+              validationError={wordsError}
+              hintText="Casa, apartamento, etc."
+              floatingLabelText="Tipo de Endereço"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <Formsy.Form
+            name="state"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              name="route"
+              type="text"
+              value="client/state/"
+              hidden
+              readOnly
+            / >
+            <FormsyText
+              name="name"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Estado do endereço"
+              floatingLabelText="Estado"
+              required
+            />
+            <FormsyText
+              name="abbreviation"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="DF, RS, MG ..."
+              floatingLabelText="Sigla do estado"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <Formsy.Form
+            name="country"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              name="route"
+              type="text"
+              value="client/country/"
+              hidden
+              readOnly
+            / >
+            <FormsyText
+              name="name"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Nome do país"
+              floatingLabelText="País"
+              required
+            />
+            <FormsyText
+              name="abbreviation"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="BR, US ..."
+              floatingLabelText="Sigla do país"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <Formsy.Form
+            name="bank_account"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              name="route"
+              hidden
+              readOnly
+              type="text"
+              value="client/bank-account/"
+            / >
+            <FormsyText
+              name="name"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Agência da conta bancária"
+              floatingLabelText="Agência"
+              required
+            />
+            <FormsyText
+              name="account"
+              validations="isNumeric"
+              validationError={numericError}
+              hintText="Número da conta bancária"
+              floatingLabelText="Conta"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <Formsy.Form
+            name="dependent"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              name="route"
+              type="text"
+              value="client/dependent/"
+              hidden
+              readOnly
+            / >
+            <FormsyText
+              name="name"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Nome do dependente"
+              floatingLabelText="Nome"
+              required
+            />
+            <FormsyText
+              name="surname"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Sobrenome do dependente"
+              floatingLabelText="Sobrenome"
+              required
+            />
+            <FormsyDate
+              name="birthday"
+              hintText="Data de nasc. do cliente"
+              floatingLabelText="Data de Nascimento"
+              required
+            />
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <Formsy.Form
+            name="active_client"
+            onValid={this.enableButton.bind(this)}
+            onInvalidSubmit={this.notifyFormError}
+            onValidSubmit={submitForm.bind(this)}>
+            <input
+              hidden
+              readOnly
+              name="route"
+              type="text"
+              value="client/active/"
+            / >
+            <IconButton
+              name="id_document"
+              tooltip="Documento de Identificação"
+              touch={true}
+              tooltipPosition="top-left">
+              <FileFileUpload />
+            </IconButton>
+            <IconButton
+              name="proof_of_address"
+              tooltip="Comprovante de Residência"
+              touch={true}
+              tooltipPosition="top-right">
+              <FileFileUpload />
+            </IconButton>
+            <button hidden name="partial_submit" type="submit" />
+          </Formsy.Form>
+
+          <RaisedButton
+            primary
+            type="submit"
+            label="Enviar"
+            onClick={partialSubmitForms.bind(this)}
+            disabled={!this.state.canSubmit}
+          />
+        </Paper>
       </div>
     ); 
   }
