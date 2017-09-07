9611fb38486925e00add5bd6800c99c6631deaab
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-09-07 18:19:50 -0300

    Create a form to patrimony

--- src/patrimony/PatrimonyForm.js
+++ src/patrimony/PatrimonyForm.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
+import {Form} from 'formsy-react';
+import {FormsyText} from 'formsy-material-ui/lib';
+import {Auth} from '../auth/Auth';
 
 export default class Employeer extends Component {
 
+  submit(data){
+    fetch('/api/patrimony/',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: Auth.getHeader(),
+      }
+    ).then((e) => e.json()) 
+     .then((e) => console.log(e));
+  }
+
   render() {
     return (
-      <div>
-        oi
-      </div>
+      <Form onValidSubmit={this.submit}
+        onValid={() => true}
+        onInvalid={() => true}
+        onInvalidSubmit={() => console.log('Deu ruim')}
+      >
+        <FormsyText
+          name='fgts'
+          validations='isNumeric'
+          validationError='Esse campo precisa ser numérico'
+          hintText='O quanto você recebe de fgts?'
+          floatingLabelText='FGTS'
+        />
+        <button type='submit'>x</button>
+      </Form>
     );
   }
 }
