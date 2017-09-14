23c04d6f42ddc8897800b1babe50ebd305185045
Marcelo Ferreira <marcelohpf@gmail.com>
2017-09-14 14:51:57 -0300

    Fix many times call render for bug with DatePicker
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/App.js
+++ src/App.js
@@ -11,31 +11,27 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {'auth': false, 'begin': Date.now()};
-    this.updateDate = this.updateDate.bind(this);
-    this.logOutHandle = this.logOutHandle.bind(this);
+    this.state = {'auth': false};
+    this.handleTimeLogout = this.handleTimeLogout.bind(this);
   }
 
-  updateDate(){
-    this.setState({'begin': Date.now()});
-  }
-
-  logOutHandle(time){
-    console.log(Date.now() - this.state.begin, time);
-    if(Auth.isAuthenticated() && Date.now() - this.state.begin > time) {
-      Auth.deauthenticate();
+  handleTimeLogout(){
+    if (!Auth.isAuthenticated() && this.state.auth){
+      this.setState({auth: false});
+    } else if (Auth.isAuthenticated() && !this.state.auth) {
+      this.setState({auth: true});
     }
   }
 
   componentDidMount(){
-    setInterval(()=>{ this.setState({'auth': Auth.isAuthenticated()});}, 1000);
 
     /* Add validation to logout when user don not make some moviment */
     const body = document.getElementsByTagName('body')[0];
-    body.onmousemove = this.updateDate;
-    body.onkeyup = this.updateDate;
-    const oneMinute = 1000*60; // 1000 ms * 60 s = 1 min
-    setInterval(this.logOutHandle, oneMinute, oneMinute*15);
+    body.onmousemove = Auth.updateDate;
+    body.onkeyup = Auth.updateDate;
+    // TODO: Remove it with when add the flux pattern 
+    const second = 1000; // 1000 ms = 1 sec
+    setInterval(this.handleTimeLogout, second);
   }
 
   render() {
