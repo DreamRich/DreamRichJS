e4aa426f7759ab936601afdf7633f9c1b9f0e804
Marcelo Ferreira <marcelohpf@gmail.com>
2017-09-15 16:52:45 -0300

    Adjust in post function to log and enable a handle
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/resources/Requests.js
+++ src/resources/Requests.js
@@ -19,22 +19,25 @@ const getData = (url, component, field, fieldB) => {
   .catch((error) => {console.log(error);});
 };
 
-const postData = (url, component, data) => {
-  fetch(routeMap[this.name], {
+const postData = (url, data, component, handleResponse) => {
+  fetch(url, {
     method: methods.POST,
     headers: getAuthenticatedHeader(),
     body: JSON.stringify(data),
   })
     .then((response) => {
       if(response.ok) {
-        console.log(this.name + ' was submitted');
+        console.log(url + ' was submitted');
+        return response.json();
       } else {
-        throw new Error (this.name + ' could not be submitted');
+        throw new Error (`Response error ${response.status}: `+
+          `${url} could not be submitted`);
       }
     })
+    .then(handleResponse)
     .catch((error) => {
-      console.error(error.message);
+      console.error(error);
     });
-}
+};
 
-export default getData;
+export {getData, postData};
