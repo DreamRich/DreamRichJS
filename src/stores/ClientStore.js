c609823db196a7b180821f399257ccada901a6bf
Sabryna de Sousa <sabryna.sousa1323@gmail.com>
2017-09-29 11:30:37 -0300

    Moving submition form to client store
     Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
     Signed-off-by: Sabryna de Sousa <sabryna.sousa1323@gmail.com>

--- src/stores/ClientStore.js
+++ src/stores/ClientStore.js
@@ -2,7 +2,7 @@
 
 import {ReduceStore} from 'flux/utils';
 import AppDispatcher from '../AppDispatcher';
-//import {postData} from '../resources/Requests';
+import {postData} from '../resources/Requests';
 //import {Auth} from '../auth/Auth';
 import ActionType from '../actions/ActionType';
 
@@ -15,6 +15,12 @@ class ClientStore extends ReduceStore {
     switch (action.actionType) {
     case ActionType.CLIENT.ACTIVE:
       console.log(action.data);
+      postData('/api/client/active/',
+        action.data,
+        (data) => {
+          console.log(data);
+        }
+      );
       return state;
     default:
       console.log(action);
