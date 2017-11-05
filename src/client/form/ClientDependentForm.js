18d160c16a6cad78ac7f2cb04040666c3015dd92
eduardonunes <eduardonunes2525@gmail.com>
2017-11-05 21:35:31 +0000

    Add new way to add elements in dependents

--- src/client/form/ClientDependentForm.js
+++ src/client/form/ClientDependentForm.js
@@ -5,11 +5,13 @@ import errorMessages from '../../utils/FormsErrorMessages';
 import SubForm from '../../components/SubForm';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
-import CardForm from '../../components/CardForm';
 import ActionType from '../../actions/ActionType';
 import ClientStore from '../../stores/ClientStore';
 import AppDispatcher from '../../AppDispatcher';
 import getSelectOption from '../../utils/getSelectOption';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 var {
   wordsError,
@@ -50,84 +52,101 @@ export default class ClientDependentForm extends Component {
     key: key,
   })
 
-  getContentCard(dependent){
-    return (
-      <Row around="xs">
-        <Col xs>
-          <FormsyText
-            name="name"
-            validations="isWords"
-            validationError={wordsError}
-            hintText="Nome do dependente"
-            floatingLabelText="Nome"
-            value={dependent.name}
-          />
-        </Col>
-        <Col xs>
-          <FormsyText
-            name="surname"
-            validations="isWords"
-            validationError={wordsError}
-            hintText="Sobrenome do dependente"
-            floatingLabelText="Sobrenome"
-            value={dependent.surname}
-          />
-        </Col>
-        <Col xs>
-          <FormsyDate
-            name="birthday"
-            floatingLabelText="Data de Nascimento"
-            value={dependent.birthday}
-          />
-        </Col>
-      </Row>
-    );
-  }
-
-
-  render = () => {
-    const subtitleCard = 'Insira as informações correspondentes as ' +
-     'informações do dependente.';
-    const labelAdd = (this.state.dependents.length === 0 ?
-      'O cliente possui dependentes? (Marque o quadrado ao lado caso haja).' :
-      'O cliente possui mais dependentes? (Marque o quadrado ao lado' +
-      ' caso haja).');
+  getContentCard(dependent, index){
     const labelRemove='O cliente possui não dependentes? '+
       '(Desmarque o quadrado ao lado caso não haja).';
 
     return (
       <div>
-        {this.state.dependents.map( (dependent) => {
-          //const dependent = this.state.dependents[index];
-          //console.log(index);
-          const index = dependent.index;
-          return (
-            <div key={index}>
-              {getSelectOption(
-                this.removeDependent.bind(this, index), true, labelRemove)
-              }
-              <SubForm
-                name="dependent"
-                action={ActionType.CLIENT.POSTMULTIFORM}
-                title="Dependente"
-                parent_name='active_client_id'
-                parent_id={this.props.id}
-                index={index}
-                canSubmit={this.props.canSubmit}
-              >
-                <CardForm
-                  titleCard='Dependentes'
-                  subtitleCard={subtitleCard}
-                  contentCard={this.getContentCard(dependent)}
-                />
-              </SubForm>
-            </div>
-          );
-        })
-        }
-
-        {getSelectOption(this.addDependent, false, labelAdd)}
+        <Row around="xs">
+          <Col xs>
+            <FormsyText
+              name="name"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Nome do dependente"
+              floatingLabelText="Nome"
+              value={dependent.name}
+            />
+          </Col>
+          <Col xs>
+            <FormsyText
+              name="surname"
+              validations="isWords"
+              validationError={wordsError}
+              hintText="Sobrenome do dependente"
+              floatingLabelText="Sobrenome"
+              value={dependent.surname}
+            />
+          </Col>
+          <Col xs>
+            <FormsyDate
+              name="birthday"
+              floatingLabelText="Data de Nascimento"
+              value={dependent.birthday}
+              isFormDisabled={false}
+            />
+          </Col>
+          <Col xs>
+            {getSelectOption(
+              this.removeDependent.bind(this, index), true, labelRemove)
+            }
+          </Col>
+        </Row>
       </div>
+    );
+  }
+
+  addElement(){
+    return (
+      <Row around="xs">
+        <Col xs>
+        </Col>
+        <Col xs>
+          <FloatingActionButton key='0' onClick={this.addDependent} >
+            <ContentAdd />
+          </FloatingActionButton>
+        </Col>
+        <Col xs>
+        </Col>
+      </Row>
+    );
+  }
+
+  render = () => {
+    const subtitleCard = 'Insira as informações correspondentes as ' +
+     'informações do dependente.';
+
+    return (
+      <Card className='Card' >
+        <CardTitle
+          title='Dependente'
+          subtitleCard={subtitleCard}
+        />
+        <CardText>
+          <div>
+            {this.state.dependents.map( (dependent) => {
+              const index = dependent.index;
+              return (
+                <div key={index}>
+                  <SubForm
+                    name="dependent"
+                    action={ActionType.CLIENT.POSTMULTIFORM}
+                    title="Dependente"
+                    parent_name='active_client_id'
+                    parent_id={this.props.id}
+                    index={index}
+                    canSubmit={this.props.canSubmit}
+                  >
+                    {this.getContentCard(dependent, index)}
+                  </SubForm>
+                </div>
+              );
+            })}
+          </div>
+          {this.addElement()}
+        </CardText>
+      </Card>
     );
   }
 }
