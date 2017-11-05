c9e4252e1d94bcf71e028c0507c22c1ad8157d7b
eduardonunes <eduardonunes2525@gmail.com>
2017-11-05 12:24:25 +0000

    Add dropdown in sidebar

--- src/layout/SidebarMenu.js
+++ src/layout/SidebarMenu.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Drawer from 'material-ui/Drawer';
-//import Divider from 'material-ui/Divider';
 import makeMenuItem from '../utils/makeMenuItem';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import PersonAdd from 'material-ui/svg-icons/social/person-add';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {white} from 'material-ui/styles/colors';
 import {Link} from 'react-router-dom';
 import MenuItem from 'material-ui/MenuItem';
-import LogoutComponent from '../auth/LogoutComponent';
 import {AuthorizedLink} from '../routes/Router';
-
-
-//{makeMenuItem('change_own_client_data','/register/client','New Client', <PersonAdd color={white} />, styleText)}
-
-// TODO: Make css in a extern file
-//const muiTheme = getMuiTheme({
-//  drawer: {
-//    color: '#fE4155'
-//  },
-//});
-
-const styleText = {
-//  color: 'white',
-//  textAlign: 'left',
-//  fontSize: 14
-};
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentSend from 'material-ui/svg-icons/content/send';
 
 export default class SidebarMenu extends React.Component {
 
@@ -34,37 +17,81 @@ export default class SidebarMenu extends React.Component {
     navDrawerOpen: PropTypes.bool.isRequired,
     auth: PropTypes.bool.isRequired,
   }
+  state = {
+    open: false,
+  };
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
 
   render() {
     return (
       <Drawer open={this.props.navDrawerOpen}>
-        {makeMenuItem('allow_any','/',<h1>DreamRich</h1>,'',styleText, 
-          'MenuItem')}
-        {makeMenuItem('change_own_client_data','/register/steps',
-          'Steps Client', <PersonAdd color={white} />, styleText, 'MenuItem')}
-        {makeMenuItem('see_all_basic_client_data','/client','Client',
-          <PersonAdd color={white} />, styleText, 'MenuItem')}
-        {makeMenuItem('see_employee_data','/employee','Employee',
-          <PersonAdd color={white} />,styleText, 'MenuItem')}
-        {makeMenuItem('allow_any','/goals','Goal',
-          <PersonAdd color={white} />,styleText, 'MenuItem')}
-
-        {!this.props.auth && <Link to='/login'>
-          <MenuItem 
+        <List className='MenuItem'>
+          {makeMenuItem('allow_any','/',<h1>DreamRich</h1>,'',
+            'MenuItem')}
+          {makeMenuItem('change_own_client_data','/register/steps',
+            'Steps Client', <PersonAdd color={white} />, 'MenuItem')}
+          {makeMenuItem('see_all_basic_client_data','/client','Client',
+            <PersonAdd color={white} />, 'MenuItem')}
+          {makeMenuItem('allow_any','/goals','Goal',
+            <PersonAdd color={white} />, 'MenuItem')}
+          {!this.props.auth && <Link to='/login'>
+            <MenuItem
+              className='MenuItem'
+              leftIcon={<PersonAdd color={white} />}
+              primaryText='Login'
+            />
+          </Link>
+          }
+          <AuthorizedLink
+            permission='allow_any'
+            to='/dashboard/16'
+          >
+            <ListItem
+              primaryText="Dashboard Client"
+              className='MenuItem'
+              leftIcon={<ContentSend color={white} />}
+            />
+          </AuthorizedLink>
+          <ListItem
+            primaryText="Employee"
             className='MenuItem'
-            leftIcon={<PersonAdd color={white} />}
-            primaryText='Login'
+            leftIcon={<ContentInbox color={white} />}
+            initiallyOpen={false}
+            primaryTogglesNestedList={true}
+            nestedItems={[
+              <AuthorizedLink
+                key={'dropdownLinkMenu'+2}
+                permission='see_employee_data'
+                to='/employee/common'
+              >
+                <ListItem
+                  key={'dropdownLink'+1}
+                  className='MenuItem'
+                  primaryText="Common"
+                  leftIcon={<ContentInbox color={white} />}
+                />
+              </AuthorizedLink>
+              ,
+              <AuthorizedLink
+                key={'dropdownLinkMenu'+3}
+                permission='see_employee_data'
+                to='/employee/financial'
+              >
+                <ListItem
+                  key={'dropdownLink'+2}
+                  className='MenuItem'
+                  primaryText="Financial"
+                  leftIcon={<ContentInbox color={white} />}
+                />
+              </AuthorizedLink>
+            ]}
           />
-        </Link>
-        }
-        <AuthorizedLink permission='allow_any'
-          to={{}}>
-          <LogoutComponent 
-            className='MenuItem'
-            leftIcon={<PersonAdd color={white} />}
-            primaryText='Sign out'
-          />
-        </AuthorizedLink>
+        </List>
       </Drawer>
     );
   }
