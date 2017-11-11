d9a733a8eddf631690e8879f5251813eda060e6c
eduardonunes <eduardonunes2525@gmail.com>
2017-11-11 01:11:04 +0000

    Add icons in sidebar menu

--- src/layout/SidebarMenu.js
+++ src/layout/SidebarMenu.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import Drawer from 'material-ui/Drawer';
 import makeMenuItem from '../utils/makeMenuItem';
 import PersonAdd from 'material-ui/svg-icons/social/person-add';
+import Person from 'material-ui/svg-icons/social/person';
+import PersonOutline from 'material-ui/svg-icons/social/person-outline';
 import {white} from 'material-ui/styles/colors';
-import {Link} from 'react-router-dom';
-import MenuItem from 'material-ui/MenuItem';
 import {AuthorizedLink} from '../routes/Router';
 import {List, ListItem} from 'material-ui/List';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
-import ContentSend from 'material-ui/svg-icons/content/send';
+import Dashboard from 'material-ui/svg-icons/action/dashboard';
 
 export default class SidebarMenu extends React.Component {
 
@@ -36,31 +35,11 @@ export default class SidebarMenu extends React.Component {
           {makeMenuItem('change_own_client_data','/register/steps',
             'Steps Client', <PersonAdd color={white} />, 'MenuItem')}
           {makeMenuItem('see_all_basic_client_data','/client','Client',
-            <PersonAdd color={white} />, 'MenuItem')}
-          {makeMenuItem('allow_any','/goals','Goal',
-            <PersonAdd color={white} />, 'MenuItem')}
-          {!this.props.auth && <Link to='/login'>
-            <MenuItem
-              className='MenuItem'
-              leftIcon={<PersonAdd color={white} />}
-              primaryText='Login'
-            />
-          </Link>
-          }
-          <AuthorizedLink
-            permission='allow_any'
-            to='/dashboard/16'
-          >
-            <ListItem
-              primaryText="Dashboard Client"
-              className='MenuItem'
-              leftIcon={<ContentSend color={white} />}
-            />
-          </AuthorizedLink>
-          <ListItem
+            <Person color={white} />, 'MenuItem')}
+          {this.props.auth && <ListItem
             primaryText="Employee"
             className='MenuItem'
-            leftIcon={<ContentInbox color={white} />}
+            leftIcon={<PersonOutline color={white} />}
             initiallyOpen={false}
             primaryTogglesNestedList={true}
             nestedItems={[
@@ -73,7 +52,7 @@ export default class SidebarMenu extends React.Component {
                   key={'dropdownLink'+1}
                   className='MenuItem'
                   primaryText="Common"
-                  leftIcon={<ContentInbox color={white} />}
+                  style={{marginLeft:'55px'}}
                 />
               </AuthorizedLink>
               ,
@@ -86,11 +65,22 @@ export default class SidebarMenu extends React.Component {
                   key={'dropdownLink'+2}
                   className='MenuItem'
                   primaryText="Financial"
-                  leftIcon={<ContentInbox color={white} />}
+                  style={{marginLeft:'55px'}}
                 />
               </AuthorizedLink>
             ]}
           />
+          }
+          <AuthorizedLink
+            permission='allow_any'
+            to='/dashboard/16'
+          >
+            <ListItem
+              primaryText="Dashboard Client"
+              className='MenuItem'
+              leftIcon={<Dashboard color={white} />}
+            />
+          </AuthorizedLink>
         </List>
       </Drawer>
     );
