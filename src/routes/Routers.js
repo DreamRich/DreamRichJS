138c747ae67a9f8a16abe5427807ec2942355de9
Marcelo Ferreira <marcelohpf@gmail.com>
2017-11-16 20:35:15 -0200

    Modify client dependent form to use HOC
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -6,14 +6,14 @@ import ResetForm from '../auth/ResetForm';
 import {AuthorizedRoute, NotFoundRoute} from './Router';
 import ClientTable from '../client/ClientTable';
 import Employeer from '../employee/Employeer';
-import PatrimonyRegister from '../patrimony/PatrimonyRegister';
+// import PatrimonyRegister from '../patrimony/PatrimonyRegister';
 import ClientRegister from '../client/ClientRegister';
 import Dashboard from '../dashboard/Dashboard';
 import StepperClient from '../client/StepperClient';
 import {Auth} from '../auth/Auth';
 import GoalChart from '../goal/GoalChart';
-import GoalRegister from '../client/GoalRegister';
-import ActiveRegister from '../patrimony/ActiveRegister';
+// import GoalRegister from '../client/GoalRegister';
+// import ActiveRegister from '../patrimony/ActiveRegister';
 import ActiveProfit from '../patrimony/ActiveProfit';
 import Home from '../Home';
 
@@ -22,25 +22,25 @@ export default class Routers extends Component{
   render(){
     return (
       <Switch>
-        <Route exact path="/" component={ Home } />
+
+        <AuthorizedRoute exact permission="allow_any" path="/" component={ Home } />
+
         <Route exact path="/login" component={ LoginPage } />
-        <AuthorizedRoute exact permission="change_own_client_data" path="/register/goal" component={ GoalRegister } />
-        <AuthorizedRoute exact permission="change_own_client_data" path="/register/active" component={ ActiveRegister } />
-        <AuthorizedRoute exact permission="change_own_client_data" path="/register/patrimony" component={ PatrimonyRegister } />
+        <Route path="/login/resetpassword" render={ () => <ResetForm email="marcelohpf@hotmail.com" /> } />
+
         <AuthorizedRoute exact permission="change_own_client_data" path="/patrimony/active/:id" component={ ActiveProfit } />
-        <AuthorizedRoute exact permission="see_all_basic_client_data" path="/client" component={ ClientTable } />
         <AuthorizedRoute permission="see_employee_data" path="/employee" component={ Employeer } />
-        <Route exact path="/login" component={ LoginPage } />
+        <AuthorizedRoute exact permission="see_all_basic_client_data" path="/client" component={ ClientTable } />
         <AuthorizedRoute permission="change_own_client_data" exact path="/register/client" component={ ClientRegister } />
 
-        <AuthorizedRoute permission="allow_any" exact path="/dashboard/:id?" component={ Dashboard } />
+        <AuthorizedRoute exact permission="change_own_client_data" path="/register/steps/:id?" component={ StepperClient } />
+        <AuthorizedRoute exact permission="allow_any" path="/dashboard/:id?" component={ Dashboard } />
 
-        <AuthorizedRoute permission="change_own_client_data" exact path="/register/steps/:id?" component={ StepperClient } />
         <AuthorizedRoute permission="allow_any" path="/goals" component={ GoalChart } />
+
         <AuthorizedRoute permission="allow_any" path="/login/changepassword"
           render={ () => <PasswordForm userid={Auth.getUserId()} username={Auth.getUserName()} /> } />
 
-        <Route path="/login/resetpassword" render={ () => <ResetForm email="marcelohpf@hotmail.com" /> } />
         <Route component={ NotFoundRoute } />
       </Switch>
     );
