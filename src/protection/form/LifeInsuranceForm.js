7cbfa2ca2a4096fb549f0691b8abe21fee5c92e3
Marcelo Ferreira <marcelohpf@gmail.com>
2017-12-22 20:32:04 -0200

    Add 3 options of videos
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/protection/form/LifeInsuranceForm.js
+++ src/protection/form/LifeInsuranceForm.js
@@ -1,9 +1,17 @@
+import React, {Component} from 'react';
 import TableFormHOC from '../../components/tables/TableFormHOC';
 import ActionType from '../../actions/ActionType';
 import {routeMap} from '../../routes/RouteMap';
 import ProtectionStore from '../../stores/ProtectionStore';
+import RaisedButton from 'material-ui/RaisedButton';
+import PlayIcon from 'material-ui/svg-icons/av/play-circle-filled';
+import RemoveIcon from 'material-ui/svg-icons/av/not-interested';
+import {Card,
+  CardActions,
+  CardMedia,
+} from 'material-ui/Card';
 
-export default TableFormHOC(
+const TableForm = TableFormHOC(
   {
     submit: ActionType.PROTECTION.POSTMULTIFORM,
     add: ActionType.PROTECTION.ADD,
@@ -31,3 +39,59 @@ export default TableFormHOC(
     return {registers};
   }
 );
+
+export {TableForm};
+
+export default class LifeInsuranceForm extends Component {
+
+  state = {video: false, url: ''}
+
+  changeVideo = (url) => {
+    this.setState({url});
+  }
+
+
+  render = () => {
+    console.log(this.state.url, '0000');
+    return (
+      <div>
+        <Card>
+          <CardActions>
+            <RaisedButton
+              icon={<RemoveIcon />}
+              onClick={this.changeVideo.bind(this, '')}
+              label="Não exibir Video"
+              primary
+            />
+            <RaisedButton
+              icon={<PlayIcon />}
+              onClick={this.changeVideo.bind(this, 'EYWEwJWwA5w')}
+              label="1" />
+            <RaisedButton
+              icon={<PlayIcon />}
+              onClick={this.changeVideo.bind(this, 'JdyGABwBfbw')}
+              label="2" />
+            <RaisedButton
+              icon={<PlayIcon />}
+              onClick={this.changeVideo.bind(this, 'T0hb0c6gt7o')}
+              label="3" />
+          </CardActions>
+          <CardMedia>
+            {this.state.url !== '' &&
+              <iframe
+                height='320'
+                width='420'
+                src={`https://www.youtube.com/embed/${this.state.url}?iv_load_policy=3&controls=2&modestbranding=1&playsinline=0&showinfo=0`}
+                allowFullScreen='allowfullscreen'
+                frameBorder='0'
+              />
+            }
+          </CardMedia>
+        </Card>
+
+        <TableForm {...this.props} />
+      </div>
+    );
+  }
+
+}
